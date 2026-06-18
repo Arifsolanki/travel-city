3e4240726450bdacaac3fab8badeec5df5dc3624
Saqlain Ahmed <saqlain.zzz@gmail.com>
2026-06-18 11:39:32 +0530

    Made changes to the code as per feedback

--- src/controllers/city.js
+++ src/controllers/city.js
@@ -1,121 +1,126 @@
 const City = require("../models/City");
 
 const createCity = async (req, res) => {
+  let sendRes = {
+    success: false,
+    message: "Something went wrong",
+    data: null,
+  };
   try {
     const city = await City.create(req.body);
-    res.status(201).json({
-      success: true,
-      message: "City created successfully",
-      data: city,
-    });
+    sendRes.success = true;
+    sendRes.message = "City created successfully";
+    sendRes.data = city;
+    return res.status(201).send(sendRes);
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      data: null,
-    });
+    console.log("Error in creating city", error);
+    return res.status(500).send(sendRes);
   }
 };
 
 const getCities = async (req, res) => {
+  let sendRes = {
+    success: false,
+    message: "Something went wrong",
+    data: null,
+  };
   try {
     const cities = await City.find();
-    res.status(200).json({
-      success: true,
-      message: "Cities fetched successfully",
-      data: cities,
-    });
+    sendRes.success = true;
+    sendRes.message = "Cities fetched successfully";
+    sendRes.data = cities;
+    return res.status(200).send(sendRes);
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      data: null,
-    });
+    console.log("Error in getting cities", error);
+    return res.status(500).send(sendRes);
   }
 };
 
 const getCityById = async (req, res) => {
+  let sendRes = {
+    success: false,
+    message: "Something went wrong",
+    data: null,
+  };
   try {
+    if (!req.params.id) {
+      sendRes.message = "ID is required";
+      return res.status(400).send(sendRes);
+    }
     const city = await City.findById(req.params.id);
     if (!city) {
-      return res.status(404).json({
-        success: false,
-        message: "City not found",
-        data: null,
-      });
+      sendRes.message = "City not found";
+      return res.status(404).send(sendRes);
     }
-    res.status(200).json({
-      success: true,
-      message: "City fetched successfully",
-      data: city,
-    });
+    sendRes.success = true;
+    sendRes.message = "City fetched successfully";
+    sendRes.data = city;
+    return res.status(200).send(sendRes);
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      data: null,
-    });
+    console.log("Error in getting city", error);
+    return res.status(500).send(sendRes);
   }
 };
 
 const updateCity = async (req, res) => {
+  let sendRes = {
+    success: false,
+    message: "Something went wrong",
+    data: null,
+  };
   try {
+    if (!req.params.id) {
+      sendRes.message = "ID is required";
+      return res.status(400).send(sendRes);
+    }
     const city = await City.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
     if (!city) {
-      return res.status(404).json({
-        success: false,
-        message: "City not found",
-        data: null,
-      });
+      sendRes.message = "City not found";
+      return res.status(404).send(sendRes);
     }
-    res.status(200).json({
-      success: true,
-      message: "City updated successfully",
-      data: city,
-    });
+    sendRes.success = true;
+    sendRes.message = "City updated successfully";
+    sendRes.data = city;
+    return res.status(200).send(sendRes);
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      data: null,
-    });
+    console.log("Error in updating city", error);
+    return res.status(500).send(sendRes);
   }
 };
-
 
 const deleteCity = async (req, res) => {
+  let sendRes = {
+    success: false,
+    message: "Something went wrong",
+    data: null,
+  };
   try {
+    if (!req.params.id) {
+      sendRes.message = "ID is required";
+      return res.status(400).send(sendRes);
+    }
     const city = await City.findByIdAndDelete(req.params.id);
     if (!city) {
-      return res.status(404).json({
-        success: false,
-        message: "City not found",
-        data: null,
-      });
+      sendRes.message = "City not found";
+      return res.status(404).send(sendRes);
     }
-    res.status(200).json({
-      success: true,
-      message: "City deleted successfully",
-      data: null,
-    });
+    sendRes.success = true;
+    sendRes.message = "City deleted successfully";
+    sendRes.data = null;
+    return res.status(200).send(sendRes);
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      data: null,
-    });
+    console.log("Error in deleting city", error);
+    return res.status(500).send(sendRes);
   }
 };
 
-module.exports = { 
-    createCity,
-    getCities,
-    getCityById,
-    updateCity,
-    deleteCity
+module.exports = {
+  createCity,
+  getCities,
+  getCityById,
+  updateCity,
+  deleteCity,
 };
-
-
