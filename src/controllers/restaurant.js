5d11110a5e42a29b166229af660477a66d59f530
Saqlain Ahmed <saqlain.zzz@gmail.com>
2026-06-18 11:33:56 +0530

    Implemented Restaurant DELETE API

--- src/controllers/restaurant.js
+++ src/controllers/restaurant.js
@@ -94,9 +94,39 @@ const updateRestaurant = async (req, res) => {
   }
 };
 
+
+
+const deleteRestaurant = async (req, res) => {
+  let sendRes = {
+    success: false,
+    message: "Something went wrong",
+    data: null,
+  };
+  try {
+    if (!req.params.id) {
+      sendRes.message = "ID is required";
+      return res.status(400).send(sendRes);
+    }
+    const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
+    if (!restaurant) {
+      sendRes.message = "Restaurant not found";
+      return res.status(404).send(sendRes);
+    }
+    sendRes.success = true;
+    sendRes.message = "Restaurant deleted successfully";
+    sendRes.data = null;
+    return res.status(200).send(sendRes);
+  } catch (error) {
+    console.log("Error in deleting restaurant", error);
+    return res.status(500).send(sendRes);
+  }
+};
+
+
 module.exports = {
     createRestaurant,
     getRestaurants,
     getRestaurantById,
-    updateRestaurant
+    updateRestaurant,
+    deleteRestaurant
 };
