0b282bcf45d5b89c5e39b8ebc196714df2325640
Faisal jamal <fjamal862@gmail.com>
2026-06-18 12:22:13 +0530

    Implemented Review POST API

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 require('./src/models/db');
+<<<<<<< HEAD
+=======
+const userRoutes = require('./src/routes/User');
+const reviewRoutes = require('./src/routes/Review');
+>>>>>>> 8f4fa7f (Implemented Review POST API)
 
 const express = require('express');
 const cors = require('cors');
@@ -30,7 +35,12 @@ app.get('/api/health', (req, res) => {
 
 // TODO: EVERYONE CREATE YOUR ROUTES FROM HERE
 
+<<<<<<< HEAD
 
+=======
+app.use('/api/users', userRoutes);
+app.use('/api/reviews', reviewRoutes);
+>>>>>>> 8f4fa7f (Implemented Review POST API)
 
 app.use(notFound);
 
